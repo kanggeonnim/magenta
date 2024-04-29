86f3131ddb6a28eeee287ce4c169befa753baa46
kanggeonnim <gun4329@gmail.com>
2024-04-29 17:45:05 +0900

    Refactor ChordsRequestSch in models.py to change the type of 'chords' from List[str] to str and update example value to "C G Am C". Also, added "music_url_list" field to schema_extra.

--- models.py
+++ models.py
@@ -6,12 +6,14 @@ from pydantic import BaseModel
 
 # 요청시
 class ChordsRequestSch(BaseModel):
-    content: List[str]
+    # chords: List[str]
+    chords: str
+
     class Config:
         orm_mode = True
         schema_extra = {
             "example": {
-                "content": "Base Tesla Model 3 Inventory has $2,410 discount and now Qualifies for $7,500 Tax Credit"
+                "chords": "C G Am C"
             }
         }
 
@@ -21,7 +23,7 @@ class ChordsResponseSch(BaseModel):
     class Config:
         orm_mode = True
         schema_extra = {
-            "example": {
-                "summary": "openai 'gpt-3.5-turbo-0125'을 사용하여 요약한 메시지 내용 입니다",
-            }
+            "music_url_list": [
+                "https://music-url.com"
+            ]
         }
