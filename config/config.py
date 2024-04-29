3da99313ed4dbeec4689581087109bec93491fc3
kanggeonnim <gun4329@gmail.com>
2024-04-29 17:45:58 +0900

    Updated config.py to include additional secret keys and file paths for AWS integration.

--- config/config.py
+++ config/config.py
@@ -9,10 +9,14 @@ class Settings:
 
     config_secret_common = json.loads(open(CONFIG_SECRET_COMMON_FILE, encoding='utf-8').read())
 
-    # OPENAI_API_KEY: str = config_secret_common["OPENAI_API_KEY"]
-    # GPT_PROMPT_BILL: str = config_secret_common["GPT_PROMPT_BILL"]
-    # GPT_PROMPT_ISSUE: str = config_secret_common["GPT_PROMPT_ISSUE"]
-    # GPT_PROMPT_NEWS: str = config_secret_common["GPT_PROMPT_NEWS"]
+    MIDI_FILE_PATH: str = config_secret_common["MIDI_FILE_PATH"]
+    MP3_FILE_PATH: str = config_secret_common["MP3_FILE_PATH"]
+    BUNDLE_FILE_PATH: str = config_secret_common["BUNDLE_FILE_PATH"]
+    AWS_ACCESS_KEY_ID: str = config_secret_common["AWS_ACCESS_KEY_ID"]
+    AWS_SECRET_ACCESS_KEY: str = config_secret_common["AWS_SECRET_ACCESS_KEY"]
+    AWS_DEFAULT_REGION: str = config_secret_common["AWS_DEFAULT_REGION"]
+    AWS_BUCKET_NAME: str = config_secret_common["AWS_BUCKET_NAME"]
+    AWS_CLOUD_FRONT: str = config_secret_common["AWS_CLOUD_FRONT"]
 
 
 settings = Settings()
