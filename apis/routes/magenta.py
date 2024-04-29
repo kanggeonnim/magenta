06af02db4d7a98f28465481874e7d55e15866f43
kanggeonnim <gun4329@gmail.com>
2024-04-29 17:45:38 +0900

    Refactor music generation endpoint in magenta.py for improved chord processing and error handling.

--- apis/routes/magenta.py
+++ apis/routes/magenta.py
@@ -1,36 +1,20 @@
 from fastapi import APIRouter
 from starlette.responses import JSONResponse
-
 from magenta.models.improv_rnn import improv_rnn_generate
 from models import ChordsResponseSch, ChordsRequestSch
 
 router = APIRouter()
 
 
-@router.post("/music/generate", response_model=ChordsResponseSch)
-async def translate_by_gpt_router(req: ChordsRequestSch):
-    print(req)
-    print(len(req.content))
+@router.post("/music/magenta", response_model=ChordsResponseSch)
+async def generate_music_by_chords(req: ChordsRequestSch):
+    print(req.chords)
+    print(len(req.chords))
 
     # await improv_rnn_generate
-    improv_rnn_generate.main("")
-    # if not pre_prompt:
-    #     return JSONResponse(content={"error": f"Invalid category: {category}"}, status_code=400)
+    upload_key_list = improv_rnn_generate.main(req.chords)
 
-    return JSONResponse(content=req.content, status_code=200)
+    if not upload_key_list:
+        return JSONResponse(content={"error": f"Invalid chords: {req}"}, status_code=400)
 
-
-
-async def translate_by_openai(req: ChordsResponseSch, pre_prompt: str):
-    # OpenAI API 키 인증
-    # 모델 - GPT 3.5 Turbo 선택
-    model = "gpt-3.5-turbo-0125"
-
-    # 메시지 설정
-    messages = [{
-        "role": "user",
-        "content": pre_prompt + req.content,
-    }]
-
-    # ChatGPT API 호출
-    return ""
+    return JSONResponse(content={"musicUrls": upload_key_list}, status_code=200)
