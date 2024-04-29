5905dd70f0a69719c0d2438633fd58c3f5f7630f
kanggeonnim <gun4329@gmail.com>
2024-04-29 17:46:04 +0900

    Refactor API router in api_base.py to remove unnecessary prefix and add missing newline at the end of the file

--- apis/api_base.py
+++ apis/api_base.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 from apis.routes import magenta
 
 api_router = APIRouter()
-api_router.include_router(magenta.router, prefix="/ai", tags=["ai"])
+api_router.include_router(magenta.router, tags=["ai"])
