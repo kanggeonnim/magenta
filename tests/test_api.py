a6ff5a49c37773e568e1d2df6c6ea76d7e27e97d
kanggeonnim <gun4329@gmail.com>
2024-04-30 17:58:12 +0900

    Refactor test_api.py to include URL pattern matching for musicUrls in response JSON. Add test cases to validate URL patterns using regular expressions.

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,5 @@
+import re
+
 from fastapi.testclient import TestClient
 
 from main import app
@@ -6,13 +8,17 @@ client = TestClient(app)
 
 
 def test_music_generate_success():
-    response = client.get("/music/magenta", params={"chords": "C G Am G"})
+    response = client.post("/music/magenta", json={"chords": "C G Am G"}, )
     assert response.status_code == 200
-    assert response.json() == {
-        "id": "foo",
-        "title": "Foo",
-        "description": "There goes my hero",
-    }
+    assert "musicUrls" in response.json()
+
+    # 정규식 패턴을 사용하여 URL 추출
+    pattern = r"https?://(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}(?:/[^/#?]+)+\.(?:mp3)"
+    music_urls = response.json()["musicUrls"]
+
+    # 응답에 포함된 URL이 패턴과 일치하는지 확인
+    for url in music_urls:
+        assert re.match(pattern, url), f"URL {url} does not match the expected pattern."
 
 
 def test_music_generate_fail_zero_chord():
@@ -28,15 +34,4 @@ def test_music_generate_fail_one_chord():
     assert response.status_code == 400
     assert response.json() == {
         "error": "At least two Chords are required"
-    }
-
-
-def test_create_item():
-    response = client.post(
-        "/items/",
-        json={"id": "foobar", "title": "Foo Bar", "description": "The Foo Barters"},
-    )
-    assert response.status_code == 200
-    assert response.json() == {
-        "error": "Empty Chord"
     }
