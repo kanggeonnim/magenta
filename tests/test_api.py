7a22dc74230bea7b4bcd7af04ec7ca08947e007d
kanggeonnim <gun4329@gmail.com>
2024-05-02 13:13:20 +0900

    Refactor test_music_generate_success to use patch decorator to mock main function in improv_rnn_generate module

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,24 +1,23 @@
-import re
-
 from fastapi.testclient import TestClient
+from unittest.mock import patch
 
 from main import app
 
 client = TestClient(app)
 
 
-def test_music_generate_success():
-    response = client.post("/music/magenta", json={"chords": "C G Am G"}, )
+@patch("magenta.models.improv_rnn.improv_rnn_generate.main")
+def test_music_generate_success(mock_main):
+    mock_main.return_value = ["https.test-return-url.mp3"]
+    response = client.get("/music/magenta", params={"chords": "C,G,Am,G"}, )
     assert response.status_code == 200
     assert "musicUrls" in response.json()
 
     # 정규식 패턴을 사용하여 URL 추출
-    pattern = r"https?://(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}(?:/[^/#?]+)+\.(?:mp3)"
     music_urls = response.json()["musicUrls"]
-
     # 응답에 포함된 URL이 패턴과 일치하는지 확인
     for url in music_urls:
-        assert re.match(pattern, url), f"URL {url} does not match the expected pattern."
+        assert url == "https.test-return-url.mp3"
 
 
 def test_music_generate_fail_zero_chord():
